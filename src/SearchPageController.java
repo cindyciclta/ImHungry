931f87e89f664fcab09c069824f7dcf4fabbb5f6
Veda Murthy <vvmurthy@usc.edu>
2019-03-02 13:00:41 -0800

    started search page

--- src/SearchPageController.java
+++ src/SearchPageController.java
@@ -16,8 +16,22 @@ public class SearchPageController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("SearchPageView.jsp");
-		requestDispatcher.forward(request, response);
+		
+		String action = request.getParameter("action");
+		
+		// Search action
+		if(action == null || action.isEmpty() || action.equals("redirect")) {
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("SearchPageView.jsp");
+			requestDispatcher.forward(request, response);
+		}else if(action.equals("search")) {
+			// Results action
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("ResultsPageController");
+			requestDispatcher.forward(request, response);
+		}
+		
+		
+		
+		
 	}
 
 }
