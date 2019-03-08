c93eca42434d723eef96f42abb3ddf48efd91458
Cindy <clarissa@usc.edu>
2019-03-07 21:26:26 -0800

    FIXED GOOGLE IMAGES

--- src/models/GoogleImageRequestModel.java
+++ src/models/GoogleImageRequestModel.java
@@ -27,7 +27,7 @@ public class GoogleImageRequestModel {
 		imagesearch.trim();
 		String trimmed = imagesearch.replaceAll("\\_", "+");
 		//The URL to Google Image API - API key and Custom Control key included, Add the imageSearch at the end.
-		URL url = new URL("https://www.googleapis.com/customsearch/v1?key=AIzaSyBn3V9GHx2KjGQTmeUPHFN2Nm0BGko-zJ4" + 
+		URL url = new URL("https://www.googleapis.com/customsearch/v1?key=AIzaSyAXQIcDDxDyKhuBXztNpFg1qggmeNc0ySA" + 
 				"&cx=018271258977134758236:ifnf_dla9yk&q=" + trimmed + "&searchType=image");
 		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
 	    con.setRequestProperty("User-Agent", "Chrome");
@@ -40,15 +40,54 @@ public class GoogleImageRequestModel {
 		String response = "";
 		while((line = reader.readLine()) != null) {
 			response += line;
+			System.out.println(line);
 		}
 		return response;
 	}
 	
 	public ResponseCodeModel APIImageSearch(String imagesearch) {	
 		try {
-			String response = getResponse(imagesearch);
+			System.out.println("went apiimage "+ imagesearch);
+			imagesearch.trim();
+			String trimmed = imagesearch.replaceAll("\\_", "+");
+			//The URL to Google Image API - API key and Custom Control key included, Add the imageSearch at the end.
+			URL url = new URL("https://www.googleapis.com/customsearch/v1?key=AIzaSyAXQIcDDxDyKhuBXztNpFg1qggmeNc0ySA" + 
+					"&cx=018271258977134758236:ifnf_dla9yk&q=" + trimmed + "&searchType=image");
+			HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+		    con.setRequestProperty("User-Agent", "Chrome");
+			con.addRequestProperty("Referer", "localhost:8080");
+
+			//Parse JSON file and Serialize the Objects
+			String line;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String response = "";
+			//Get all data from API
+			while((line = reader.readLine()) != null) {
+				response += line;
+				System.out.println(line);
+			}
+
+		    if (!response.isEmpty()) {
+		    	//Parses the JSON object to get the image and store the url link to an ArrayList
+		    	JSONObject json = new JSONObject(response);
+		    	JSONArray items = json.getJSONArray("items");
+		    	ArrayList <String> allimageurl = new ArrayList<String>();
+				int actualsize = 0;
+		    	for (int i = 0; i < 10 ; i ++) {
+		    		JSONObject item_i = items.getJSONObject(i);
+			    	JSONObject imageobj = item_i.getJSONObject("image");
+			    	String string_urlimage = imageobj.getString("thumbnailLink");
+			    	if (string_urlimage != null || string_urlimage != "") {
+				    	allimageurl.add(string_urlimage);
+
+				    	actualsize++;
+		    		}
+		    	}
+		    	this.results.setListofImages(allimageurl, actualsize);
+		    }
+			String responsee = getResponse(imagesearch);
 	    	//Parses the JSON object to get the image and store the url link to an ArrayList
-	    	JSONObject json = new JSONObject(response);
+	    	JSONObject json = new JSONObject(responsee);
 	    	JSONArray items = json.getJSONArray("items");
 	    	ArrayList <String> allimageurl = new ArrayList<String>();
 			int actualsize = 0;
